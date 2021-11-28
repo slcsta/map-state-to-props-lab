03e9003563959ddc3c778d4f08873f3ac2678f1b
Sarah <sarahlchavez@gmail.com>
2021-11-28 14:02:02 -0800

    commit tree 39906aa946b8a17dbe5d90bcae608a315bdf3c17

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import App from './App';
 
-import manageUsers from './reducers/manageUsers'
+// use the createStore method from redux passing in the provided reducer, manageUsers to create a store
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import manageUsers from './reducers/manageUsers';
 
 
 // add imports and code
+let store = createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
+// use Provider from react-redux to wrap <App /> passing store as a prop to the Provider this will give your components access to the store
 
 ReactDOM.render(
   // add imports and code
+  <Provider store={store}>
   <App />
+  </Provider>
   // add imports and code
   ,
   document.getElementById('root')
